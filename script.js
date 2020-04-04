5f9080b4a6ca418644fa38f7330102f6cf9c435a
Julian Dolinay <dolinay1@gmail.com>
2020-04-04 07:28:13 -0400

    created layout for application using the bulma framework

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("works");
+
+// let questionChoices = Object.values(choices)[quizProperties.questionSet];
+
+//     if (quizProperties.questionSet === Object.keys(questions).length) {
+//         console.log("done");
+//     } else {
+        
+//         questionChoices.forEach(key => {
+//             let searchItemDiv = document.createElement("searchItem");
+                
+//                 searchedItemDivDisplay.append(searchItemDiv);
+
+//                 searchItemDiv.classList.add("btn", "btn-primary", "mr-3", "mt-3", "choice");
+
+//                 searchItemDiv.innerHTML = key;
+//             });
+//     }
